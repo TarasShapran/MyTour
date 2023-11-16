e8df5f399004754758c7eb86dec4758637802070
tshapran <Taras.Shapran@cfraresearch.com>
2023-11-16 21:07:50 +0200

    test5

--- market_prj/settings.py
+++ market_prj/settings.py
@@ -101,9 +101,10 @@ WSGI_APPLICATION = 'market_prj.wsgi.application'
 # }
 import dj_database_url
 DATABASES = {}
-DATABASES['default'] = dj_database_url.config(default='postgresql://postgresql', conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(default='postgresql://localhost', conn_max_age=600)
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
 DATABASES['default']['NAME'] = 'market_prj_db'
+DATABASES['default']['PORT'] = '5432'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
